ad3297997224843e66f78724db5ffea4b81c956f
S21loganmc <1940652847@qq.com>
2019-09-20 18:11:29 +0800

    First

---
+++ 456/四则运算/四则运算/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 四则运算
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Num of question：");
+            int x = int.Parse(Console.ReadLine());//确定题目个数
+
+            Console.WriteLine("Range：");
+            int y = int.Parse(Console.ReadLine());//运算范围
+
+            MathEz Ca = new MathEz(x, y);
+            Ca.Operator();
+
+            Console.WriteLine("Check or not（YorN）：");
+            char See = char.Parse(Console.ReadLine());
+            if (See == 'Y' || See == 'y')
+            {
+                Console.WriteLine("Answer Time");
+                Ca.SeeResult();
+            }
+            else
+            {
+                Console.WriteLine("THX:)");
+            }
+            string str = Console.ReadLine();
+        }
+    }
+}
---
+++ 456/四则运算/四则运算/类.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 四则运算
+{
+    public class MathEz
+    {
+        int x, y;
+        double[] Result;
+        Random rdm = new Random();
+        public MathEz(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+            Result = new double[x];
+        }
+        public void Operator()
+        {
+            for (int i = 0; i < x; i++)
+            {
+                int a = rdm.Next(y) + 1;
+                int b = rdm.Next(y) + 1;
+                char f = op();
+                if (f == '+')
+                {
+                    Console.WriteLine("{0}、{1} {2} {3} = ", i + 1, a, f, b);
+                    Result[i] = a + b;
+                }
+                else if (f == '-')
+                {
+                    if (a < b) { a = a + b; b = a - b; a = a - b; } 
+                    Console.WriteLine("{0}、{1} {2} {3} = ", i + 1, a, f, b);
+                    Result[i] = a - b;
+                }
+                else if (f == '*')
+                {
+                    Console.WriteLine("{0}、{1} {2} {3} = ", i + 1, a, f, b);
+                    Result[i] = a * b;
+                }
+                else
+                {
+                    Console.WriteLine("{0}、{1} {2} {3} = ", i + 1, a, f, b);
+                    Result[i] = Math.Round(Convert.ToDouble(a) / b, 3);
+                }
+
+            }
+        }
+        char op()
+        {
+            int k = rdm.Next(4);
+            char r = '+';
+            switch (k)
+            {
+                case 0: r = '+'; break;
+                case 1: r = '-'; break;
+                case 2: r = '*'; break;
+                case 3: r = '/'; break;
+            }
+            return r;
+        }
+        public void SeeResult()
+        {
+            foreach (double i in Result)
+            {
+                Console.WriteLine(i.ToString());
+            }
+        }
+    }
+}
